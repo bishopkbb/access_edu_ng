44193d689c745f17ced4f656c6c66e2c43643853
Ajibade Tosin Francis <ajibade_tosin@yahoo.com>
2025-09-01 14:47:15 +0100

    update vite_config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/access_edu_ng/",   // 👈 matches your GitHub repo name
+  base: "/",   
   server: {
     port: 5173,
     strictPort: true
